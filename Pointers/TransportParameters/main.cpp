c82f8852d53df33ac66fdf2a90839c0a77a5ffd1
Axenus <platon-gamer@mail.ru>
2023-03-14 01:23:04 +0500

    dz 13.03

--- Pointers/TransportParameters/main.cpp
+++ Pointers/TransportParameters/main.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 using namespace std;
-void Exchange(int a, int b);
+void Exchange(int* a, int* b);
 
 void main()
 {
@@ -8,15 +8,15 @@ void main()
 	int a = 2, b = 3;
 	cout << a << "\t" << b << endl;
 	cout << &a << "\t" << &b << endl;
-	Exchange(a,b);
+	Exchange(&a, &b);
 	cout << a << "\t" << b << endl;
 }
 
-void Exchange(int a, int b)
+void Exchange(int* a, int* b)
 {
-	int buffer = a;
-	a = b;
-	b = buffer;
-	cout << a << "\t" << b << endl;
+	int buffer = *a;
+	*a = *b;
+	*b = buffer;
+	cout << *a << "\t" << *b << endl;
 	cout << &a << "\t" << &b << endl;
 }
